1ffe63a709c5789e6881330d038dd401d9e4fe1b
asafeliasim <asafeliasim@gmail.com>
2020-06-04 06:23:21 +0300

    Make palette list responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,5 @@
+import sizes from './sizes';
+
 export default {
     root: {
         backgroundColor: "blue",
@@ -11,7 +13,14 @@ export default {
         display: 'flex',
         alignItems: 'flex-start',
         flexDirection: 'column',
-        flexWrap: 'wrap'
+        flexWrap: 'wrap',
+        [sizes.down('lg')]:{
+            width: '80%',
+        },
+        [sizes.down('xs')]:{
+            width: '75%',
+        },
+
     },
     nav:{
         display: "flex",
@@ -24,11 +33,18 @@ export default {
             textDecoration: 'none'
         }
     },
-    palettes:{
+    palettes: {
         boxSizing: "border-box",
         width: "100%",
         display: "grid",
         gridTemplateColumns: "repeat(3,30%)",
-        gridGap:"5%"
+        gridGap: "2.5rem",
+        [sizes.down('md')]: {
+            gridTemplateColumns: "repeat(2,50%)"
+        },
+        [sizes.down('xs')]: {
+            gridTemplateColumns: "repeat(1,100%)",
+            gridGap: "1rem"
+        }
     }
 };
